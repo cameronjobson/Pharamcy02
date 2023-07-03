67ba1bebc4f5434af928dae25bf82492968709af
Cameron <132536746+cameronjobson@users.noreply.github.com>
2023-07-03 20:33:25 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
+import React, { useState } from 'react';
+import Select from 'react-select';
 import './App.css';
 
+const drugs = [
+  { value: 0.15, label: 'Codeine' },
+  { value: 2.4, label: 'Fentanyl Transdermal' },
+  { value: 1.0, label: 'Hydrocodone' },
+  { value: 5.0, label: 'Hydromorphone' },
+  { value: 4.7, label: 'Methadone' },
+  { value: 1.0, label: 'Morphine' },
+  { value: 1.5, label: 'Oxycodone' },
+  { value: 3.0, label: 'Oxymorphone' },
+  { value: 0.4, label: 'Tapentadol' },
+  { value: 0.2, label: 'Tramadol' },
+];
+
 function App() {
+  const [strengthPerUnit, setStrengthPerUnit] = useState(0);
+  const [units, setUnits] = useState(0);
+  const [daysSupply, setDaysSupply] = useState(0);
+  const [mmePerDay, setMmePerDay] = useState(0);
+  const [selectedDrug, setSelectedDrug] = useState(null);
+
+  const calculateMME = () => {
+    if (!selectedDrug) return; // Don't calculate if no drug is selected
+
+    let mme = (strengthPerUnit * (units / daysSupply) * selectedDrug.value);
+    setMmePerDay(mme);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <form>
+        <input type="number" onChange={e => setStrengthPerUnit(e.target.value)} placeholder="Strength per Unit" />
+        <input type="number" onChange={e => setUnits(e.target.value)} placeholder="Units" />
+        <input type="number" onChange={e => setDaysSupply(e.target.value)} placeholder="Days Supply" />
+        <Select 
+          options={drugs} 
+          onChange={setSelectedDrug} 
+          placeholder="Select a drug" 
+          isSearchable 
+        />
+        <button type="button" onClick={calculateMME}>Calculate</button>
+        <div className="resultBox">
+            <p>MME/Day: {mmePerDay}</p>
+        </div>
+      </form>
     </div>
   );
 }
